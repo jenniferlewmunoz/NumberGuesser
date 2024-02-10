2c29e38aed9eed63e38008fe23b617c37409f09f
Jennifer J Lew Munoz <jenniferlewmunoz@gmail.com>
2024-02-09 20:26:34 -0800

    finished project

--- script.js
+++ script.js
@@ -4,3 +4,34 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
+function generateTarget() {
+    return Math.floor(Math.random() * 9);
+}
+
+function compareGuesses(humanGuess, computerGuess, target) {
+
+    let playerDistance = Math.abs(target - humanGuess);
+    let computerDistance = Math.abs(target - computerGuess);
+
+    if (playerDistance <= computerDistance) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function updateScore(winner) {
+
+    if (winner === 'human') {
+        humanScore++;
+    }
+
+    if (winner === 'computer') {
+        computerScore++;
+    }
+}
+
+function advanceRound() {
+    currentRoundNumber++;
+
+}
